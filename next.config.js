62f67a36b5bc8b548222f3dc3a5691166763ea34
marecop <yellowenterprice@outlook.com>
2025-04-19 10:22:40 +0800

    初始提交 - Yellow Airlines 網站

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+    unoptimized: process.env.NODE_ENV !== 'production',
+  },
+};
+
+module.exports = nextConfig; 
